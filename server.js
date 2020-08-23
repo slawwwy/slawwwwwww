6fe2d56185dc5e872e47b49bfc0541d783b9faf5
Glitch (hello-express) <none>
2020-08-23 13:58:23 +0000

    🚝🍩 Checkpoint ./server.js:21402697/3007 ./package.json:21402697/1037 ./config.json:21402697/170

--- server.js
+++ server.js
@@ -1,34 +1,74 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const http = require("http");
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendStatus(200);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://partner1er1.glitch.me/`);
+}, 280000);
+
+const Discord = require("discord.js");
+const config = require("./config.json");
+const ms = require("ms");
+const enmap = require("enmap");
+const moment = require("moment");
+
+const client = new Discord.Client();
+const db = new enmap({ name: "test" });
+
+const prefix = config.prefix;
+const token = config.token;
+const time = config.time;
+
+client.on("ready", () => {
+  console.log("logged in as " + client.user.username);
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+client.on("message", async message => {
+  if (message.content === 'join') {
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+  
+  if (
+    message.author.bot ||
+    message.channel.type !== "dm" ||
+    !message.content.includes("discord.gg")
+  )
+    return;
+
+  var user = message.author;
+  if (db.get(user.id) == undefined) {
+    db.ensure(user.id, {
+      last: null
+    });
+  }
+
+  if (db.get(user.id, "last") !== null && moment().diff(moment(parseInt(db.get(user.id, "last"))), "h") < time) {
+    message.channel.send(`> **You need to wait until it ends ${moment(parseInt(db.get(user.id, "last"))).add(time, "h").fromNow()}**`);
+    return;
+  }
+  
+  var ad_message = message.content.replace("@everyone", "").replace("@here", "");
+
+  client.channels.cache
+    .get(config.ad_channel)
+    .send(ad_message + "\n\n<@" + user.id + ">");
+
+
+  message.channel.send(">>> **Done.\nCheck <#"+client.channels.cache.get(config.ad_channel).id +
+      ">**"
+  );
+
+  db.set(user.id, moment().format("x"), "last");
 });
+
+client.login("");
