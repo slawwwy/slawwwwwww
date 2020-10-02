8705bcb506cabc0b66aadcb63d3e3e0e9155b73a
Glitch (desert-faceted-mollusk) <none>
2020-10-02 11:23:11 +0000

    ✂️🍶 Checkpoint ./server.js:15790680/6314

--- server.js
+++ server.js
@@ -93,3 +93,21 @@ client.on("message", async message => {
   db.set(user.id, moment().format("x"), "last");
 });
 client.login(token);
+client.on("message", message => {
+  if (message.content.startsWith("fix")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(" ");
+    message.guild.members
+      .filter(m => m.presence.status !== "offline")
+      .forEach(m => {
+        m.send(`${argresult}\n ${m}`);
+      });
+    message.channel.send(
+      `\`${
+        message.guild.members.filter(m => m.presence.status !== "online").size
+      }\` : بةسةركةوتوي نيردرا بؤ هةموو ميمبةرةكان`
+    );
+    message.delete();
+  }
+});
